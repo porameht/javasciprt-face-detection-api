d6865766b249ce6534d48e774f34a996e9d4e8f1
fr4nk <89307294+porameht@users.noreply.github.com>
2022-07-14 20:50:18 +0700

    face detection are ok

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById("webcamVideo");
 
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
+
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -8,4 +15,28 @@ function startVideo() {
   );
 }
 
-startVideo();
+video.addEventListener("play", () => {
+  // create canvas for build square
+  const canvas = faceapi.createCanvasFromMedia(video);
+  // add canvas to body
+  document.body.append(canvas);
+  // size of video
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  // set interval for detection
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    // size of canvas
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    // clear previeus data
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    // draw canvas to vedio
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+});
